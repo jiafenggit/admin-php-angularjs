28b3c01c8571bc5b32fdfda23f04b7ab948afc4e
chan <chanunlang@gmail.com>
2016-10-07 21:37:59 +0800

    0

---
+++ server/server/application/models/admin/Admin_user_model.php
@@ -0,0 +1,107 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin_user_model extends MY_Model {  
+
+  protected $_rules = array(
+    'username' => array(
+      'field'=>'username',
+      'label'=>'用户名',
+      'rules'=>'trim|required',
+      'errors' => array('required' => '{field}未设置')
+    ),
+    'name' => array(
+      'field'=>'name',
+      'label'=>'昵称',
+      'rules'=>'trim|required',
+      'errors' => array('required' => '{field}未设置')
+    ),
+    'password' => array(
+      'field'=>'password',
+      'label'=>'密码',
+      'rules'=>'trim|required|min_length[5]|md5',
+      'errors' => array('required' => '{field}未设置', 'min_length' => '{field}不能低于{param}位数') 
+    ),
+    'role' => array(
+      'field'=>'role',
+      'label'=>'权限组',
+      'rules'=>'trim|required|numeric',
+      'errors' => array('required' => '{field}未设置', 'numeric' => '请输入正确的{field}')
+    )
+  );
+  protected $_tbl = 'admin_info';
+  protected $_tbl_key = 'uid';  
+  protected $_query_field = 'uid,username,name,role';
+  protected $_get_field = 'uid,username,name,role,ip,utime,ctime';
+  protected $_create_field = 'username,name,role';
+  protected $_update_field = 'username,name,role';
+
+  public function __construct()
+  {  
+    parent::__construct();    
+  }
+
+  function create($resource)
+  { 
+    $valid = $this->validation($resource,'create');
+    if($valid['status'] === true)
+    {
+      $resource = $valid['resource'];
+      $hasuser = $this->db
+        ->where('username',$resource['username'])
+        ->count_all_results($this->_tbl) > 0;
+      if($hasuser)
+      {
+        $valid = array(
+          'status' => false,
+          'error' => array('用户名已存在')
+        );
+      }
+      $resource['ctime'] = $resource['utime'] = time();
+      $data['ip'] =ip2long($this->input->ip_address());
+      $resource['status'] = 1;
+      $this->db->insert($this->_tbl, $resource);
+      return array('status' => true);
+    }
+    return $valid;
+  }
+
+  public function auth($req)
+  { 
+    $this->_create_field = 'username,password';
+    $valid = $this->validation($resource,'auth');
+    if($valid['status'] === true)
+    {  
+      $resource = $valid['resource'];
+      $results = $this->db->from($this->tbl)
+        ->select('uid,username,password,name,role')
+        ->where('username',$resource['username'])
+        ->where('status',1)
+        ->get()
+        ->result_array();
+      if(count($results) < 1)
+      {  
+        $valid = array(
+          'status' => false,
+          'errors' => array('用户不存在')
+        );
+      }
+      else if($results[0]['password'] !== $resource['password'])
+      {
+        $valid = array(
+          'status' => false,
+          'errors' => array('密码不正确')
+        );
+      }
+      else
+      { 
+        $user = $results[0];
+        unset($user['password']);
+        $valid = array(
+          'status' => false,
+          'info' => $user
+        );
+      }
+    }
+    return $valid;
+  }
+}
